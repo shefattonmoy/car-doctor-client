9e7b2ce25320790e6b2007ee1d720a86a38a5b24
Shefat Tonmoy <shefathossain7@gmail.com>
2023-05-17 23:45:47 +0600

    some components added

---
+++ src/Components/Checkout/Checkout.jsx
@@ -0,0 +1,81 @@
+import { useContext } from "react";
+import { useLoaderData } from "react-router-dom";
+import { AuthContext } from "../Providers/AuthProvider/AuthProvider";
+
+const Checkout = () => {
+    const service = useLoaderData();
+    const { _id, title, img } = service;
+    const {user} = useContext(AuthContext);
+
+    const handleCheckout = (event) => {
+        event.preventDefault();
+        const form = event.target;
+        const name = form.name.value;
+        const date = form.date.value;
+        const email = user?.email;
+        const phone = form.phone.value;
+        const booking = {
+            customerName: name,
+            customerEmail: email,
+            date,
+            img,
+            service: title,
+            service_id: _id,
+            phone
+        }
+        console.log(booking);
+
+        fetch('http://localhost:5000/bookings', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(booking)
+        })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+        })
+    }
+    return (
+        <div>
+            <h2 className="text-center text-3xl font-bold">Service: {title}</h2>
+            <form onSubmit={handleCheckout}>
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text"></span>
+                        </label>
+                        <input type="text" name="name" placeholder="Name" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text"></span>
+                        </label>
+                        <input type="date" name="name" placeholder="Service Date" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text"></span>
+                        </label>
+                        <input type="number" name="phone" placeholder="Your Phone" className="input input-bordered" />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text"></span>
+                        </label>
+                        <input type="email" name="email" defaultValue={user?.email} placeholder="Your Email" className="input input-bordered" />
+                    </div>
+                </div>
+                <div className="form-control mt-6">
+                    <input className="btn btn-error btn-block" type="submit" value="Confirm Order" />
+                </div>
+            </form>
+            <div className="card-body">
+
+            </div>
+        </div>
+    );
+};
+
+export default Checkout;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -1,6 +1,9 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../Components/Layout/Layout";
 import Home from "../Components/Pages/Home/Home/Home";
+import Login from "../Components/Pages/Login/Login";
+import Registration from "../Components/Pages/Registration/Registration";
+import Checkout from "../Components/Checkout/Checkout";
 
 const router = createBrowserRouter([
     {
@@ -10,6 +13,19 @@ const router = createBrowserRouter([
         {
             path: '/',
             element: <Home></Home>
+        },
+        {
+          path: '/login',
+          element: <Login></Login>
+        },
+        {
+          path: '/signup',
+          element: <Registration></Registration>
+        },
+        {
+          path: '/checkout/:id',
+          element: <Checkout></Checkout>,
+          loader: ({params}) => fetch(`http://localhost:5000/services/${params.id}`)
         }
       ]
     },
